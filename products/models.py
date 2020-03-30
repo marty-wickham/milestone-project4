f0ed65ec71a3955a4c62bee53406effe055ba93d
martycistudent <marty.wickham@gmail.com>
2020-03-30 20:11:31 +0000

    Added more data to game catalogue, slight style changes to navbar

--- products/models.py
+++ products/models.py
@@ -33,6 +33,8 @@ class Game(models.Model):
         ('18', '18'),
     ]
 
+    DISCOUNT_CHOICES = [(i, i) for i in range(5, 96, 5)]
+
     name = models.CharField(max_length=120, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=4, decimal_places=2)
@@ -43,16 +45,17 @@ class Game(models.Model):
     works_on = models.CharField(max_length=7, choices=WORKS_ON_CHOICES, blank=False)
     release_date = models.DateField()
     company = models.CharField(max_length=30)
-    size = models.DecimalField(max_digits=3, decimal_places=1)
     pegi_rating = models.CharField(max_length=2, choices=PEGI_RATING_CHOICES, blank=True)
+    sale = models.BooleanField(blank=True, default=False)
+    discount = models.IntegerField(blank=True, choices=DISCOUNT_CHOICES)
 
     def __str__(self):
         return self.name
 
 
 class Review(models.Model):
-    """Model migration design for votes"""
-    STAR_CHOICES = [(i, i) for i in range(1, 5)]
+    """Model migration design for reviews"""
+    STAR_CHOICES = [(i, i) for i in range(1, 6)]
 
     game = models.ForeignKey(Game, null=False)
     user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
