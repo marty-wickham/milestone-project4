757ca4045c73e55185f7da588dd9ea4a2ac0bd4f
martycistudent <marty.wickham@gmail.com>
2020-04-08 13:21:46 +0000

    Fixed layout issues in cart.html, products.html. Made new css classes, Wrote stripes js script

--- checkout/views.py
+++ checkout/views.py
@@ -34,7 +34,7 @@ def checkout(request):
                     quantity=quantity
                 )
                 order_line_item.save()
-            
+
             try:
                 customer = stripe.Charge.create(
                     amount=int(total * 100),
@@ -44,19 +44,19 @@ def checkout(request):
                 )
             except stripe.error.CardError:
                 messages.error(request, "Your card was declined!")
-            
+
             if customer.paid:
                 messages.error(request, "You have successfully paid")
                 request.session['cart'] = {}
                 return redirect(reverse('products'))
             else:
                 messages.error(request, "Unable to make card payment")
-            
+
         else:
             print(payment_form.errors)
             messages.error(request, "We were unable to take payment with that card!")
     else:
         payment_form = MakePaymentForm()
         order_form = OrderForm()
-    
+
     return render(request, "checkout.html", {'order_form': order_form, 'payment_form': payment_form, 'publishable': settings.STRIPE_PUBLISHABLE})
