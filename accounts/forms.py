363ec7c5b945c7b87fcad0b2f3d36fbbd809f11f
martycistudent <marty.wickham@gmail.com>
2020-04-21 12:47:27 +0000

    Updated Django to 2.1.5, fixed database issue, project models all working, adding UserUpdate forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.core.exceptions import ValidationError
+from .models import Profile
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -18,3 +18,9 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['prof_image']
