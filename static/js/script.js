a8a10d58437f06542a6726b1a54f84cea5d2516c
martycistudent <marty.wickham@gmail.com>
2020-06-05 13:26:11 +0000

    Fixing star rating functionality in review section

--- static/js/script.js
+++ static/js/script.js
@@ -1,54 +1,4 @@
 $(document).ready(function () {
-    var hover = true;
-
-    $(".box").click(function () {
-        if (hover === true) {
-            hover = false;
-            console.log("hover is " + hover)
-            // var rating = $(this).atrr("id");
-
-            // $("#id_rating").val(rating);
-            return hover
-        } else {
-            hover = true;
-            console.log("hover is " + hover)
-            return hover
-        }
-
-
-    });
-    console.log("hover is " + hover)
-
-
-    if (hover === true) {
-        $("#1").mouseenter(function () {
-            $(this).addClass("highlight");
-            $("#2").removeClass("highlight");
-        });
-
-        $("#2").mouseenter(function () {
-            $("#1, #2").addClass("highlight");
-            $("#3").removeClass("highlight");
-        });
-
-        $("#3").mouseenter(function () {
-            $("#1, #2, #3").addClass("highlight");
-            $("#4").removeClass("highlight");
-        });
-
-        $("#4").mouseenter(function () {
-            $("#1, #2, #3, #4").addClass("highlight");
-            $("#5").removeClass("highlight");
-        });
-
-        $("#5").mouseenter(function () {
-            $("#1, #2, #3, #4, #5").addClass("highlight");
-        });
-
-        $("#star-container").mouseleave(function () {
-            $(".box").removeClass("highlight");
-        });
-    }
 
 
     display = false;
@@ -70,5 +20,36 @@ $(document).ready(function () {
         video = $("#video");
         video.pause();    
     });
-});
 
+
+    // $(".box").on( "mouseenter", function() {
+    //     var onStar = parseInt($(this).data('rating'), 10); // The star currently hovering over
+
+    //     $(this).parent().children(".box").each(function(e){
+    //         if (e < onStar) {
+    //             $(this).addClass("highlight");
+    //         }
+    //         else {
+    //             $(this).removeClass("highlight");
+    //         }
+    //     });
+    
+    // }).on("mouseout", function(){
+    //     $(this).parent().children(".box").each(function(e){
+    //         $(this).removeClass("highlight");
+    //     });
+    // });
+
+    $( ".box" ).mouseenter(function() {
+        var onStar = parseInt($(this).data('rating'), 10); // The star currently hovering over
+
+        $( ".box" ).each(function( index, element ) {
+            // element == this
+            $( element ).addClass("highlight");
+            if ( $( this ).is( onStar ) ) {
+            
+                return false;
+            }
+        });
+    });
+});
