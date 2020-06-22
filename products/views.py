fb69f9e7e5e203d6ce0ba4381d0d5d59e5fdc957
martycistudent <marty.wickham@gmail.com>
2020-06-22 15:53:06 +0000

    Fixed issue allowing users to leave multiple reviews on a game. Minor styl changes to navbar.

--- products/views.py
+++ products/views.py
@@ -3,8 +3,6 @@ from .models import Game, Review
 from .forms import ReviewForm
 from django.contrib import messages
 
-# Create your views here.
-
 
 def all_games(request):
     """Create a view that will return a list of all of the games available to
@@ -21,29 +19,33 @@ def game_details(request, pk):
     review"""
     game = get_object_or_404(Game, pk=pk)
     reviews = Review.objects.filter(game=game).order_by("-date_posted")
+    user_review = Review.objects.filter(user=request.user, game=game)
+    review_count = game.reviews.count()
+    sum = 0
+    avg_rating = 0
+
+    if review_count > 0:
+
+        for rating in game.reviews.values("rating"):
+            sum += rating["rating"]
+        avg_rating = round(sum / review_count, 1)
 
     if request.method == 'POST':
         review_form = ReviewForm(request.POST)
 
         if review_form.is_valid():
-            review = review_form.save(commit=False)
-            review.user = request.user
-            review.game = game
-            review.save()
-            messages.success(request, f'Your review has been created!')
-            return redirect('game_details', game.pk)
+
+            if user_review:
+                messages.error(request, "You have already given your review on this game.")
+            else:
+                review = review_form.save(commit=False)
+                review.user = request.user
+                review.game = game
+                review.save()
+                messages.success(request, f'Your review has been created!')
+                return redirect('game_details', game.pk)
     else:
         review_form = ReviewForm()
-
-        review_count = game.reviews.count()
-        sum = 0
-        avg_rating = 0
-
-        if review_count > 0:
-
-            for rating in game.reviews.values("rating"):
-                sum += rating["rating"]
-            avg_rating = round(sum / review_count, 1)
 
     return render(request, 'products/game-details.html', {
         'game': game,
