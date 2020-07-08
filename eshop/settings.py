b261fee3ad4a616877b76bab5f6ce9e12b625b48
martycistudent <marty.wickham@gmail.com>
2020-07-08 11:19:39 +0000

    Started deployment on Heroku, Fixing cart layout on mobile screens

--- eshop/settings.py
+++ eshop/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get('HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('HOSTNAME'), "eshop-project4.herokuapp.com/"]
 
 
 # Application definition
