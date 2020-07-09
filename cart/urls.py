873f0f884b0d25c9b81063b3048da4d096474001
martycistudent <marty.wickham@gmail.com>
2020-07-09 14:47:55 +0000

    Fixed layout issues for cart page

--- cart/urls.py
+++ cart/urls.py
@@ -1,3 +1,4 @@
+# from django.urls import path
 from django.conf.urls import url
 from .views import view_cart, add_to_cart, adjust_cart
 
@@ -6,3 +7,9 @@ urlpatterns = [
     url(r'^add/(?P<id>\d+)', add_to_cart, name='add_to_cart'),
     url(r'^adjust/(?P<id>\d+)', adjust_cart, name='adjust_cart'),
 ]
+
+# urlpatterns = [
+#     path('', view_cart, name='view_cart'),
+#     path('add/<int:pk>/', add_to_cart, name='add_to_cart'),
+#     path('adjust/(?P<id>\d+)', adjust_cart, name='adjust_cart'),
+# ]
