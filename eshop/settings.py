de809dae4d12f0f3dbb6424b3c60eac8ccd0bbbd
martycistudent <marty.wickham@gmail.com>
2020-08-01 11:38:23 +0000

    Added Heroku domain to ALLOWED_HOSTS

--- eshop/settings.py
+++ eshop/settings.py
@@ -31,9 +31,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get('HOSTNAME')]
+ALLOWED_HOSTS = [
+    os.environ.get('HOSTNAME'),
+    "eshop-project4.herokuapp.com"]
 
 
 # Application definition
